38ba93dec73e9d900108781d79c4f3243cb38084
pontura <pontura@gmail.com>
2015-05-27 19:04:26 -0300

    borrar

--- game/Assets/LaneObjectData.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class LaneObjectData {
-
-    public string word;
-    public int score;
-
-}
--- game/Assets/TextsData.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class TextsData : MonoBehaviour {
-
-    public string SoundsOffConfirmation = "This game is based on listening and learning. Are you sure you want to turn sounds off?";
-
-}
